fba2b95fb9edc1acaac21368727e0a5cce712a33
Tomas Puricelli <tomasp1189@gmail.com>
2019-08-05 06:08:19 +0200

    Add insertion sort

---
+++ lib/algorithms/insertionSort/README.md
@@ -0,0 +1,9 @@
+# Insertion Sort
+
+Sort array one value at a time by inserting value in the correct place shifting all bigger values one place to the right.
+
+## Analysis of Algorithm (Big O Notation)
+
+### Time Complexity
+
+O(nˆ2)
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 require('./lib/BinarySearchTree/BinarySearchTree.test');
 require('./lib/algorithms/bubbleSort/bubbleSort.test');
 require('./lib/algorithms/selectionSort/selectionSort.test');
+require('./lib/algorithms/insertionSort/insertionSort.test');
